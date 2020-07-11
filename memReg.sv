2edd3a1e5fcd74c52cae38990369325ef491ab89
tsg3 <estebandcg1999@gmail.com>
2020-07-11 06:48:28 -0600

    Se añade la logica secuencial para leer los registros

--- memReg.sv
+++ memReg.sv
@@ -1,4 +1,4 @@
-module memReg(input logic reg_rd, reg_wr, clk,
+module memReg(input logic rd_allow, wr_allow, clk,
 				  input logic [3:0] Rd, Rs1, Rs2,
 				  input logic [31:0] DI,
 				  output logic [31:0] D1, D2);
@@ -6,18 +6,18 @@ module memReg(input logic reg_rd, reg_wr, clk,
 	logic [15:0][31:0] memory = 512'd0;
 	
 	always_ff @(negedge clk) begin
-		if (reg_wr)
+		if (wr_allow)
 			memory[Rd][31:0] <= DI;
 	end
 	
-	always_comb begin
-		if (reg_rd) begin
-			D1 = memory[Rs1][31:0];
-			D2 = memory[Rs2][31:0];
+	always_ff @(posedge clk) begin
+		if (rd_allow) begin
+			D1 <= memory[Rs1][31:0];
+			D2 <= memory[Rs2][31:0];
 		end
 		else begin
-			D1 = 32'd0;
-			D2 = 32'd0;
+			D1 <= 32'd0;
+			D2 <= 32'd0;
 		end
 	end
 
