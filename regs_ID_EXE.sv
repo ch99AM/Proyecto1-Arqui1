a61da69ce14d3398080679ac5eeeff9cd1c50724
tsg3 <estebandcg1999@gmail.com>
2020-07-12 22:59:07 -0600

    Se añade el selector de unidad funcional

--- regs_ID_EXE.sv
+++ regs_ID_EXE.sv
@@ -1,9 +1,9 @@
 module regs_ID_EXE (input logic clk, wr_allow,
-						  input logic [3:0] in_Rd, in_Rs1, in_Rs2, 
+						  input logic [3:0] in_Rd, in_Rs1, in_Rs2, unit_in,
 						  input logic [31:0] in_D1, in_D2, 
 						  input logic [18:0] in_imm_2R1, 
 						  input logic [25:0] in_imm_RI, 
-						  output logic [3:0] out_Rd, out_Rs1, out_Rs2, 
+						  output logic [3:0] out_Rd, out_Rs1, out_Rs2, unit_out, 
 						  output logic [31:0] out_D1, out_D2, 
 						  output logic [18:0] out_imm_2R1, 
 						  output logic [25:0] out_imm_RI);
@@ -15,6 +15,7 @@ module regs_ID_EXE (input logic clk, wr_allow,
 	logic [31:0] D2 = 32'd0;
 	logic [21:3] imm_2R1 = 19'd0;
 	logic [25:0] imm_RI = 26'd0;
+	logic [3:0] unit = 4'd0;
 	
 	assign out_Rd = Rd;
 	assign out_Rs1 = Rs1;
@@ -23,6 +24,7 @@ module regs_ID_EXE (input logic clk, wr_allow,
 	assign out_D2 = D2;
 	assign out_imm_2R1 = imm_2R1;
 	assign out_imm_RI = imm_RI;
+	assign unit_out = unit;
 	
 	always_ff @(negedge clk) begin
 		if (wr_allow) begin
@@ -33,6 +35,7 @@ module regs_ID_EXE (input logic clk, wr_allow,
 				D2 <= in_D2;
 				imm_2R1 <= in_imm_2R1;
 				imm_RI <= in_imm_RI;
+				unit <= unit_in;
 		end
 	end
 
